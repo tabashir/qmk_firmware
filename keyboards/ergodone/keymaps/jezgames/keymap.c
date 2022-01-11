8940a33cabfe111e78fb2cd48a109f6c64989b37
Jez McKinley <jez.mckinley@cwdynamics.com>
2022-01-11 22:49:24 +0000

    JM: x and y double and triple tap example

--- keyboards/ergodone/keymaps/jezgames/keymap.c
+++ keyboards/ergodone/keymaps/jezgames/keymap.c
@@ -58,8 +58,33 @@ enum {
     TD_P_U,
     TD_LBRC_U,
     TD_SH_NUBS,
-    TD_SH_TAB
+    TD_SH_TAB,
+    X_CTL,
+    SOME_OTHER_DANCE
 };
+
+typedef enum {
+    TD_NONE,
+    TD_UNKNOWN,
+    TD_SINGLE_TAP,
+    TD_SINGLE_HOLD,
+    TD_DOUBLE_TAP,
+    TD_DOUBLE_HOLD,
+    TD_DOUBLE_SINGLE_TAP, // Send two single taps
+    TD_TRIPLE_TAP,
+    TD_TRIPLE_HOLD
+} td_state_t;
+
+typedef struct {
+    bool is_press_action;
+    td_state_t state;
+} td_tap_t;
+
+td_state_t cur_dance(qk_tap_dance_state_t *state);
+
+// For the x tap dance. Put it here so it can be used in any keymap
+void x_finished(qk_tap_dance_state_t *state, void *user_data);
+void x_reset(qk_tap_dance_state_t *state, void *user_data);
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -269,7 +294,7 @@ XXXXXXX               , XXXXXXX       , XXXXXXX
 [6] = LAYOUT_ergodox(
 // left hand
 
-TD(TD_ESC_ZERO)         , TD(TD_1_F1)    , TD(TD_2_F2) , TD(TD_3_F3)    , TD(TD_4_F4)   , TD(TD_5_F5)    , TD(TD_6_F6)       ,
+TD(TD_ESC_ZERO)         , TD(X_CTL)    , TD(TD_2_F2) , TD(TD_3_F3)    , TD(TD_4_F4)   , TD(TD_5_F5)    , TD(TD_6_F6)       ,
 TD(TD_SH_TAB)           , TD(TD_SH_Q)    , TD(TD_SH_W) , TD(TD_SH_E)    , TD(TD_SH_R)   , TD(TD_SH_T)    , TD(TD_GRAVE_TAB)  ,
 TD(TD_NLCK_SLCK_TOGGLE) , TD(TD_SH_A)    , TD(TD_SH_S) , TD(TD_SH_D)    , TD(TD_SH_F)   , TD(TD_SH_G)    ,
 KC_LSFT                 , TD(TD_SH_NUBS) , TD(TD_SH_Z) , TD(TD_SH_X)    , TD(TD_SH_C)   , TD(TD_SH_V)    , TD(TD_SH_B)       ,
@@ -464,6 +489,83 @@ void insert_copy_paste(qk_tap_dance_state_t *state, void *user_data) {
   }
 }
 
+/* Return an integer that corresponds to what kind of tap dance should be executed.
+ *
+ * How to figure out tap dance state: interrupted and pressed.
+ *
+ * Interrupted: If the state of a dance dance is "interrupted", that means that another key has been hit
+ *  under the tapping term. This is typically indicitive that you are trying to "tap" the key.
+ *
+ * Pressed: Whether or not the key is still being pressed. If this value is true, that means the tapping term
+ *  has ended, but the key is still being pressed down. This generally means the key is being "held".
+ *
+ * One thing that is currenlty not possible with qmk software in regards to tap dance is to mimic the "permissive hold"
+ *  feature. In general, advanced tap dances do not work well if they are used with commonly typed letters.
+ *  For example "A". Tap dances are best used on non-letter keys that are not hit while typing letters.
+ *
+ * Good places to put an advanced tap dance:
+ *  z,q,x,j,k,v,b, any function key, home/end, comma, semi-colon
+ *
+ * Criteria for "good placement" of a tap dance key:
+ *  Not a key that is hit frequently in a sentence
+ *  Not a key that is used frequently to double tap, for example 'tab' is often double tapped in a terminal, or
+ *    in a web form. So 'tab' would be a poor choice for a tap dance.
+ *  Letters used in common words as a double. For example 'p' in 'pepper'. If a tap dance function existed on the
+ *    letter 'p', the word 'pepper' would be quite frustating to type.
+ *
+ * For the third point, there does exist the 'TD_DOUBLE_SINGLE_TAP', however this is not fully tested
+ *
+ */
+td_state_t cur_dance(qk_tap_dance_state_t *state) {
+    if (state->count == 1) {
+        if (state->pressed) return TD_SINGLE_HOLD;
+        else return TD_SINGLE_TAP;
+    } else if (state->count == 2) {
+        if (state->pressed) return TD_DOUBLE_HOLD;
+        else return TD_DOUBLE_TAP;
+    }
+
+    // Assumes no one is trying to type the same letter three times (at least not quickly).
+    // If your tap dance key is 'KC_W', and you want to type "www." quickly - then you will need to add
+    // an exception here to return a 'TD_TRIPLE_SINGLE_TAP', and define that enum just like 'TD_DOUBLE_SINGLE_TAP'
+    if (state->count == 3) {
+        /* if (state->interrupted || !state->pressed) return TD_TRIPLE_TAP; */
+        return TD_TRIPLE_TAP;
+    } else return TD_UNKNOWN;
+}
+
+// Create an instance of 'td_tap_t' for the 'x' tap dance.
+static td_tap_t xtap_state = {
+    .is_press_action = true,
+    .state = TD_NONE
+};
+
+void x_finished(qk_tap_dance_state_t *state, void *user_data) {
+    xtap_state.state = cur_dance(state);
+    switch (xtap_state.state) {
+        case TD_SINGLE_TAP: register_code(KC_X); break;
+        case TD_SINGLE_HOLD: register_code(KC_X); break;
+        case TD_DOUBLE_TAP: tap_code(KC_X); tap_code(KC_X); break;
+        case TD_DOUBLE_HOLD: register_code(KC_Y); break;
+        case TD_TRIPLE_TAP: tap_code(KC_X); tap_code(KC_X); tap_code(KC_X); break;
+        default: register_code(KC_X); break;
+    }
+}
+
+void register_key(uint16_t keycode, qk_tap_dance_state_t *state) {
+
+// to save
+
+}
+
+void x_reset(qk_tap_dance_state_t *state, void *user_data) {
+    switch (xtap_state.state) {
+        case TD_DOUBLE_HOLD: unregister_code(KC_Y);
+        default: unregister_code(KC_X); break;
+    }
+    xtap_state.state = TD_NONE;
+}
+
 qk_tap_dance_action_t tap_dance_actions[] = {
   [TD_LAYER_FN] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, dance_layers, dance_layers_finish),
   [TD_INSERT_COPY_PASTE] = ACTION_TAP_DANCE_FN(insert_copy_paste),
@@ -522,5 +624,6 @@ qk_tap_dance_action_t tap_dance_actions[] = {
   [TD_CTL_F10_ALT_F10] = ACTION_TAP_DANCE_DOUBLE(LCTL(KC_F10), LALT(KC_F10)),
   [TD_CTL_F11_ALT_F11] = ACTION_TAP_DANCE_DOUBLE(LCTL(KC_F11), LALT(KC_F11)),
   [TD_CTL_F12_ALT_F12] = ACTION_TAP_DANCE_DOUBLE(LCTL(KC_F12), LALT(KC_F12)),
-  [TD_CTL_F13_ALT_F13] = ACTION_TAP_DANCE_DOUBLE(LCTL(KC_F13), LALT(KC_F13))
+  [TD_CTL_F13_ALT_F13] = ACTION_TAP_DANCE_DOUBLE(LCTL(KC_F13), LALT(KC_F13)),
+  [X_CTL] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, x_finished, x_reset)
 };
